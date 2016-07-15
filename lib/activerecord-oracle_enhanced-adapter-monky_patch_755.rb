285f7ac940ad91bd55936f07ecf4653b44fffb46
Koichi ITO <koic.ito@gmail.com>
2016-07-15 11:41:10 +0900

    Refactoring

--- lib/activerecord-oracle_enhanced-adapter-monky_patch_755.rb
+++ lib/activerecord-oracle_enhanced-adapter-monky_patch_755.rb
@@ -2,6 +2,7 @@ module OracleEnhanced
   class MonkeyPatch755Railtie < ::Rails::Railtie
     ActiveSupport.on_load(:active_record) do
       require 'activerecord/monkey_patch'
+      require 'activerecord/monkey_patch/rails4'
     end
   end
 end
--- lib/activerecord/monkey_patch.rb
+++ lib/activerecord/monkey_patch.rb
@@ -5,9 +5,6 @@
 #
 module ActiveRecord
   module MonkeyPatch
-    MINIMUM_SUPPORTED_VERSION = '4.2.1'.freeze
-    MAXIMUM_SUPPORTED_VERSION = '4.2.7'.freeze
-
     def adjust_timezone_offset(opts)
       if ActiveRecord::Base.connection_config[:adapter] != 'oracle_enhanced' ||
         Gem::Version.create(Rails.version) < Gem::Version.create(MINIMUM_SUPPORTED_VERSION) ||
@@ -42,49 +39,5 @@ module ActiveRecord
     end
 
     module_function :adjust_timezone_offset
-  end
-
-  #
-  # Original code is the following URL.
-  #
-  #   https://github.com/rails/rails/blob/v4.2.6/activerecord/lib/active_record/relation/query_methods.rb#L947
-  #
-  module QueryMethods
-    def build_where(opts, other = [])
-      case opts
-      when String, Array
-        [@klass.send(:sanitize_sql, other.empty? ? opts : ([opts] + other))]
-      when Hash
-        opts = ActiveRecord::MonkeyPatch.adjust_timezone_offset(opts)
-        opts = PredicateBuilder.resolve_column_aliases(klass, opts)
-
-        tmp_opts, bind_values = create_binds(opts)
-        self.bind_values += bind_values
-
-        attributes = @klass.send(:expand_hash_conditions_for_aggregates, tmp_opts)
-        add_relations_to_bind_values(attributes)
-
-        PredicateBuilder.build_from_hash(klass, attributes, table)
-      else
-        [opts]
-      end
-    end
-  end
-
-  #
-  # Original code is the following URL.
-  #
-  #   https://github.com/rails/rails/blob/v4.2.6/activerecord/lib/active_record/statement_cache.rb#L81
-  #
-  class StatementCache # :nodoc:
-    class BindMap # :nodoc:
-      def bind(values)
-        values = ActiveRecord::MonkeyPatch.adjust_timezone_offset(values)
-
-        bvs = @bind_values.map { |pair| pair.dup }
-        @indexes.each_with_index { |offset,i| bvs[offset][1] = values[i] }
-        bvs
-      end
-    end
   end
 end
